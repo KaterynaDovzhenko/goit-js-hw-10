8db01c33db9217f780b004fc2126f82f7aa8525e
KaterynaDovzhenko <102299487+KaterynaDovzhenko@users.noreply.github.com>
2025-01-14 00:24:05 +0000

    Deploying to gh-pages from @ KaterynaDovzhenko/goit-js-hw-10@781a5a949adfc1eed2a6d2ce0bf56736fb868c09 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/styles-CB_RmJAn.js";import{i as s}from"./assets/vendor-A92OCY9B.js";const i=document.querySelector("[name=delay]"),c=document.querySelector("[type=submit]"),n=document.querySelector("[value=fulfilled]"),m=t=>{t.preventDefault(),new Promise((e,l)=>{console.log("robymo zapyt na server");const o=i.value;setTimeout(()=>{n.checked?e(s.show({message:`✅ Fulfilled promise in ${o}ms`})):l(s.show({message:`❌ Rejected promise in ${o}ms`}))},o)}).then(e=>{console.log(e)}).catch(e=>{console.log(e)})};c.addEventListener("click",m);
+//# sourceMappingURL=2-snackbar.js.map
